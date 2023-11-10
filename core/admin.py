20be6677ef5753324578bf0f716afa8b9bfbef1c
ephraimphrase <ephraimphrase@gmail.com>
2023-11-10 08:05:23 +0100

    added network and comment models alongside corresponding referencing in network rating model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Network, NetworkRating, Comment
+
 
 # Register your models here.
+admin.site.register(Network)
+admin.site.register(NetworkRating)
+admin.site.register(Comment)
